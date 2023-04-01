51ca59d2dacfd95caca479ea22e3cb7e94882939
CatboyPlain <september_91@inbox.ru>
2023-04-01 20:40:44 +0300

    123

--- src/js/script.js
+++ src/js/script.js
@@ -27,14 +27,58 @@ function signUp(event) {
   userPanel.style.display = 'flex';
 }
 
-const button = document.querySelector('.footer__input-arrow');
-const form = document.querySelector('form');
-button.addEventListener('click', function () {
-  console.log('клиик на кнопку');
-  form.submit();
-})
 
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  console.log('Форма отправлена');
-})
+const inputFrom = document.querySelector("#date-from");
+const inputTo = document.querySelector("#date-to");
+const dataPickerContainer = document.querySelector(".find-room__datapicker_inline");
+const dataPicker = new AirDatepicker('.find-room__datapicker_inline', {
+  range: true,
+  dynamicRange: true,
+  inline: false,
+  visible: false,
+  minDate: new Date(),
+  buttons: [{
+    content: "очистить",
+    tagName: "div",
+    onClick: (dataPicker) => {
+      dataPicker.clear();
+      dataPickerContainer.style.display = "none";
+    }
+  },
+  {
+    content: "Применить",
+    tagName: "div",
+    onClick: (dataPicker) => {
+      dataPickerContainer.style.display = "none";
+    }
+  }],
+  //   dateFormat(date) {
+  //     return date.toLocaleString('ru', {
+  //         year: 'numeric',
+  //         day: '2-digit',
+  //         month: 'long'
+  //     });
+  // }
+});
+
+
+function toggleDatePicker (event) {
+  console.log(event.target);
+  if (dataPickerContainer && dataPicker) {
+    if (event.target !== inputFrom && event.target !== inputTo) {
+      dataPickerContainer.style.display = 'none';
+      console.log("return")
+      return
+    }
+    if (getComputedStyle(dataPickerContainer).display === 'none') {
+      dataPickerContainer.style.display = 'inline-block'
+      console.log('NONA?');
+    } else if (getComputedStyle(dataPickerContainer).display === 'block') {
+        dataPickerContainer.style.display = 'none'
+      }
+    }
+  }
+
+// inputFrom.addEventListener("click", toggleDatePicker);
+// inputTo.addEventListener("click", toggleDatePicker);
+document.addEventListener("click", toggleDatePicker)
