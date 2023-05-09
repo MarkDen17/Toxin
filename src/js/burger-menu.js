6060515c07a0bb6d8a10a8cdd4b0f4a0d03498ac
MarkDen17 <september_91@inbox.ru>
2023-05-09 16:06:44 +0300

    burger menu script separated

---
+++ src/js/burger-menu.js
@@ -0,0 +1,16 @@
+const burgerIcon = document.querySelector('.burger-icon');
+const headerMenu = document.querySelector('.header__menu');
+burgerIcon.addEventListener("click", function () {
+  if (burgerIcon && burgerIcon.dataset.open) {
+    if (burgerIcon.dataset.open === "false") {
+      burgerIcon.dataset.open = "true";
+      headerMenu.classList.toggle('_active')
+      return
+    };
+    if (burgerIcon.dataset.open === "true") {
+      burgerIcon.dataset.open = "false"
+      headerMenu.classList.toggle('_active')
+      return
+    };
+  }
+})
--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,4 @@
 "use strict"
-
-const burgerIcon = document.querySelector('.burger-icon');
-const headerMenu = document.querySelector('.header__menu');
-burgerIcon.addEventListener("click", function () {
-  if (burgerIcon && burgerIcon.dataset.open) {
-    if (burgerIcon.dataset.open === "false") {
-      burgerIcon.dataset.open = "true";
-      headerMenu.classList.toggle('_active')
-      return
-    };
-    if (burgerIcon.dataset.open === "true") {
-      burgerIcon.dataset.open = "false"
-      headerMenu.classList.toggle('_active')
-      return
-    };
-  }
-})
-
 const inputFrom = document.querySelector("#date-from");
 const inputTo = document.querySelector("#date-to");
 const dataPickerContainer = document.querySelector(".find-room__datapicker_inline");
