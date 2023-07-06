f8b3198c5b64b703ffdbf3a8665150f5079bddf9
MarkDen17 <september_91@inbox.ru>
2023-07-06 17:06:41 +0300

    some accessibility added

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -14,3 +14,19 @@ burgerIcon.addEventListener("click", function () {
     };
   }
 })
+burgerIcon.addEventListener("keydown", function (event) {
+  if (event.key === 'Enter') {
+    if (burgerIcon && burgerIcon.dataset.open) {
+      if (burgerIcon.dataset.open === "false") {
+        burgerIcon.dataset.open = "true";
+        headerMenu.classList.toggle('_active')
+        return
+      };
+      if (burgerIcon.dataset.open === "true") {
+        burgerIcon.dataset.open = "false"
+        headerMenu.classList.toggle('_active')
+        return
+      };
+    }
+  }
+})
